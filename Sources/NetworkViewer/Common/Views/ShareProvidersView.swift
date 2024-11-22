15f7304a482be48b74bdc6312b34dad01b920e54
sakhabaevegor <sakhabaevegor@gmail.com>
2024-11-22 08:20:36 +0700

    fix: fix continuous share data processing

--- Sources/NetworkViewer/Common/Views/ShareProvidersView.swift
+++ Sources/NetworkViewer/Common/Views/ShareProvidersView.swift
@@ -8,33 +8,30 @@
 import SwiftUI
 
 struct ShareProvidersView: View {
+
     let operations: [NetworkViewer.Operation]
-    @State private var shareData: ShareService.Result?
     @Environment(\.shareService) private var shareService: ShareService
-    
+
     var body: some View {
         Menu {
             ForEach(shareService.providers, id: \.displayName) { provider in
-                if let shareItem = provider.shareData(for: operations) {
-                    Button {
-                        shareData = shareItem
-                    } label: {
-                        Label {
-                            Text(provider.displayName)
-                        } icon: {
-                            if let image = provider.icon {
-                                Image(uiImage: image)
-                            }
+                Button {
+                    if let shareData = provider.shareData(for: operations)?.value {
+                        let controller = UIActivityViewController(activityItems: [shareData], applicationActivities: nil)
+                        UIApplication.topViewController()?.present(controller, animated: true)
+                    }
+                } label: {
+                    Label {
+                        Text(provider.displayName)
+                    } icon: {
+                        if let image = provider.icon {
+                            Image(uiImage: image)
                         }
                     }
-                    .foregroundColor(.blue)
                 }
             }
         } label: {
             Image(systemName: "square.and.arrow.up")
-        }
-        .sheet(item: $shareData) { data in
-            ActivityViewController(activityItems: [data.get()])
         }
     }
 }
